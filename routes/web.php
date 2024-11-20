eb802afa00f2961dfc0bfd84a0e9eaebb4daa0f6
Malith Kuruppu <info.mkappworks@gmail.com>
2024-11-20 19:08:58 +0100

    Add authentication routes for registration and login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
+
+Route::get('/register', [RegistrationController::class, 'create']);
+Route::post('/register', [RegistrationController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::delete('/logout', [SessionController::class, 'destroy']);
